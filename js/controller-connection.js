3a960b995f704d47e2fdba3e7d9d87faa71f99f0
matumoto-yu <songbenyuzai@gmail.com>
2023-04-17 02:13:24 +0900

    add style change

--- js/controller-connection.js
+++ js/controller-connection.js
@@ -1,113 +1,128 @@
 
+
+window.onload = function() {
+
+    var hand_height = document.getElementById("hand").getBoundingClientRect().top;
+    var topbar_height = document.getElementsByClassName("topbar")[0].clientHeight;
+    
+
+    setTimeout(() => {
+        window.scrollTo({
+            top: (window.pageYOffset + hand_height - topbar_height),
+            behavior: "smooth",
+            });
+    }, 500);
+    
+}
+
+
 window.addEventListener("gamepadconnected", function(e) {
     var gamepad = navigator.getGamepads()[e.gamepad.index];
     console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
       gamepad.index, gamepad.id,
       gamepad.buttons.length, gamepad.axes.length);
-  });
-
+});
+ 
   
   
-  
-  
-  var presstype = false;
-  function gameLoop() {
-    var gamepad = navigator.getGamepads()[0];
-    
-    if (gamepad) {
-      var data = gamepad.buttons;
-       switch (true) {
+var presstype = false;
+function gameLoop() {
+var gamepad = navigator.getGamepads()[0];
 
-            // ぷよ回転
-            case data[0].pressed && !presstype:
-                // console.log("0 press");
+if (gamepad) {
+    var data = gamepad.buttons;
+    switch (true) {
 
-                keypress_action({key: 'k',keyCode: 75});
-                break;
-            case data[1].pressed && !presstype:
-                // console.log("1 press");
+        // ぷよ回転
+        case data[0].pressed && !presstype:
+            // console.log("0 press");
 
-                keypress_action({key: 'l',keyCode: 76});
-                break;
+            keypress_action({key: 'k',keyCode: 75});
+            break;
+        case data[1].pressed && !presstype:
+            // console.log("1 press");
 
-            
-            // 十字ボタン
-            case data[14].pressed && !presstype:
-                // console.log("14 press left");
+            keypress_action({key: 'l',keyCode: 76});
+            break;
 
-                keypress_action({key: 'v',keyCode: 86});
-                break;
-            case data[13].pressed && !presstype:
-                // console.log("13 press bottom");
+        
+        // 十字ボタン
+        case data[14].pressed && !presstype:
+            // console.log("14 press left");
 
-                keypress_action({key: 'b',keyCode: 66});
-                break;
-            case data[15].pressed && !presstype:
-                // console.log("15 press right");
+            keypress_action({key: 'v',keyCode: 86});
+            break;
+        case data[13].pressed && !presstype:
+            // console.log("13 press bottom");
 
-                keypress_action({key: 'n',keyCode: 78});
-                break;
+            keypress_action({key: 'b',keyCode: 66});
+            break;
+        case data[15].pressed && !presstype:
+            // console.log("15 press right");
 
-            
-            //戻る、進む
-            case data[4].pressed && !presstype:
-                // console.log("4 press");
+            keypress_action({key: 'n',keyCode: 78});
+            break;
 
-                keypress_action({key: 'g',keyCode: 71});
-                break;
-            case data[5].pressed && !presstype:
-                // console.log("5 press");
+        
+        //戻る、進む
+        case data[4].pressed && !presstype:
+            // console.log("4 press");
 
-                keypress_action({key: 'h',keyCode: 72});
-                break;
+            keypress_action({key: 'g',keyCode: 71});
+            break;
+        case data[5].pressed && !presstype:
+            // console.log("5 press");
 
-            
-            // 開始、リトライ
-            case data[9].pressed && !presstype:
-                // console.log("9 press");
+            keypress_action({key: 'h',keyCode: 72});
+            break;
 
-                keypress_action({key: 's',keyCode: 83});
-                break;
+        
+        // 開始、リトライ
+        case data[9].pressed && !presstype:
+            // console.log("9 press");
 
-            case data[8].pressed && !presstype:
-                // console.log("8 press");
+            keypress_action({key: 's',keyCode: 83});
+            break;
 
-                keypress_action({key: 'r',keyCode: 82});
-                break;
+        case data[8].pressed && !presstype:
+            // console.log("8 press");
 
-            
-            // エディター、とこぷよ
-            case data[10].pressed && !presstype:
+            keypress_action({key: 'r',keyCode: 82});
+            break;
 
-                document.getElementById("editor_button").click();
-                break;
+        
+        // エディター、とこぷよ
+        case data[10].pressed && !presstype:
 
-            case data[11].pressed && !presstype:
+            document.getElementById("editor_button").click();
+            break;
 
-                document.getElementById("tokopuyo_button").click();
-                break;
+        case data[11].pressed && !presstype:
 
-            case !data[0].pressed && !data[1].pressed && !data[14].pressed && !data[13].pressed && 
-                 !data[15].pressed && !data[4].pressed && !data[5].pressed && !data[9].pressed && 
-                 !data[8].pressed && !data[10].pressed && !data[11].pressed:
-                // console.log("repress");
-                presstype = false;
-                break;
-       }
-      
+            document.getElementById("tokopuyo_button").click();
+            break;
+
+        case !data[0].pressed && !data[1].pressed && !data[14].pressed && !data[13].pressed && 
+                !data[15].pressed && !data[4].pressed && !data[5].pressed && !data[9].pressed && 
+                !data[8].pressed && !data[10].pressed && !data[11].pressed:
+            // console.log("repress");
+            presstype = false;
+            break;
     }
-    requestAnimationFrame(gameLoop);
-  }
-  
-  requestAnimationFrame(gameLoop);
-  
+    
+}
+requestAnimationFrame(gameLoop);
+}
+
+requestAnimationFrame(gameLoop);
 
 
-  function keypress_action(config) {
+
+function keypress_action(config) {
 
     var keydownEvent = new KeyboardEvent('keydown', config);      
     document.dispatchEvent(keydownEvent);
-    
+
     presstype = true;
 
-  }
+}
